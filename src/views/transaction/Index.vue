442db99be7cb4164754cb8ba260a772f6427e10f
R4MA26 <ramaismaya25@gmail.com>
2021-11-04 18:13:09 +0800

    telkom-wa-api

--- src/views/transaction/Index.vue
+++ src/views/transaction/Index.vue
@@ -35,20 +35,24 @@
           Layanan Online Pasang Baru Indihome Makassar
         </h2>
 
-        <div class="p-4 d-grid gap-2">
-          <p>
-            Silahkan pilih paket Internet Indihome sesuai dengan kebutuhan Anda. Apabila
-            Anda ingin daftar Indihome atau mempunyai pertanyaan seputar layanan Indihome
-            Anda bisa langsung live chat via Whatsapp ke Marketing kami. Kami siap
-            membantu Anda sampai dengan terpasanganya layanan Indihome fiber di rumah,
-            apartemen, kantor maupun tempat usaha Anda. Kami melayani pemasangan Indihome
-            untuk seluruh wilayah Makassar, Gowa, Maros dan sekitarnya. Indihome
-            menawarkan koneksi internet cepat dan stabil dengan teknologi fiber optic.
-            Saat ini, tersedia layanan Triple Play dan Dual Play yang bisa Anda pilih.
-            Triple Play terdiri dari Internet Fiber, Telepon Rumah, dan TV Interaktif
-            (Usee TV). Adapun Dual Play terdiri dari Internet Fiber dan Telepon Rumah atau
-            Internet Fiber dan TV Interaktif (Usee TV).
-          </p>
+        <div class="p-3 d-grid gap-2">
+          <div>
+            <p style="text-align: justify">
+              Silahkan pilih paket Internet Indihome sesuai dengan kebutuhan Anda. Apabila
+              Anda ingin daftar Indihome atau mempunyai pertanyaan seputar layanan
+              Indihome Anda bisa langsung live chat via Whatsapp ke Marketing kami. Kami
+              siap membantu Anda sampai dengan terpasanganya layanan Indihome fiber di
+              rumah, apartemen, kantor maupun tempat usaha Anda. Kami melayani pemasangan
+              Indihome untuk seluruh wilayah Makassar, Gowa, Maros dan sekitarnya.
+            </p>
+            <p style="text-align: justify">
+              Indihome menawarkan koneksi internet cepat dan stabil dengan teknologi fiber
+              optic. Saat ini, tersedia layanan Triple Play dan Dual Play yang bisa Anda
+              pilih. Triple Play terdiri dari Internet Fiber, Telepon Rumah, dan TV
+              Interaktif (Usee TV). Adapun Dual Play terdiri dari Internet Fiber dan
+              Telepon Rumah atau Internet Fiber dan TV Interaktif (Usee TV).
+            </p>
+          </div>
 
           <a
             class="btn btn-success"
@@ -230,6 +234,6 @@ export default {
 
 <style scope>
 div .mt-6 {
-  margin-top: 6rem;
+  margin-top: 5rem;
 }
 </style>
